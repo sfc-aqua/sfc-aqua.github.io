0413a477a21d8566fb3da051eb458c85260d4e86
zigen <hrlclb@gmail.com>
2023-07-30 18:33:07 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const IS_SANDBOX = process.env.BUILD_ENV == 'sandbox';
 const nextConfig = {
     output: 'export',
     distDir: 'out',
+    basePath: IS_SANDBOX ? '/sfc-aqua.github.io-sandbox' : "",
     // experimental: {
     //   mdxRs: true,
     // },
