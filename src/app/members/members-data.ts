6005ea9c6d38dacece1621001e99a7387c3965ac
PorametPat <43396870+PorametPat@users.noreply.github.com>
2023-08-05 02:12:54 +0900

    Update members-data.ts

--- src/app/members/members-data.ts
+++ src/app/members/members-data.ts
@@ -35,20 +35,20 @@ export const members: TMember[] = [
   //     { name: "Luck", value: 100, max: 100 },
   //   ],
   // },
-  // {
-  //   name: "Poramet",
-  //   role: "D1",
-  //   imagePath: "/images/members/poramet.jpg",
-  //   description: "I'm a student at Keio University",
-  //   links: [
-  //     { name: "Facebook", url: "" },
-  //     { name: "Github", url: "" },
-  //   ],
-  //   stat: [
-  //     { name: "Strength", value: 20, max: 100 },
-  //     { name: "Agility", value: 30, max: 100 },
-  //     { name: "Intelligence", value: 50, max: 100 },
-  //     { name: "Luck", value: 10, max: 100 },
-  //   ],
-  // },
+  {
+    name: "Poramet",
+    role: "D1",
+    imagePath: "/images/members/poramet.jpg",
+    description: "I'm a student at Keio University",
+    links: [
+      { name: "Facebook", url: "" },
+      { name: "Github", url: "" },
+    ],
+    stat: [
+      { name: "Strength", value: 20, max: 100 },
+      { name: "Agility", value: 30, max: 100 },
+      { name: "Intelligence", value: 50, max: 100 },
+      { name: "Luck", value: 10, max: 100 },
+    ],
+  },
 ];
