273e1c5846a258e59fc53906fecd730b9207fbd2
HarukaSakate <haruka_sakate@keio.jp>
2023-10-19 12:04:53 +0900

    logic update

--- src/app/members/members-data.ts
+++ src/app/members/members-data.ts
@@ -282,4 +282,23 @@ export const members: TMember[] = [
       { name: "Luck", value: 98, max: 100 },
     ],
   },
+  {
+    name: "Haruka Sakate",
+    login: "l0gic",
+    role: "B1",
+    imagePath: "/images/members/logic.jpg",
+    description: "I'm a student at Keio University",
+    links: [
+      {
+        name: "Github",
+        url: "https://github.com/HarukaSakate",
+      },
+    ],
+    stat: [
+      { name: "Strength", value: 60, max: 100 },
+      { name: "Agility", value: 70, max: 100 },
+      { name: "Intelligence", value: 30, max: 100 },
+      { name: "Luck", value: 98, max: 100 },
+    ],
+  },
 ]
