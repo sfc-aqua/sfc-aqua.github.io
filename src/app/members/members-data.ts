a98915ac4a9ddd7949a4ef77ac0eaddf23d33cd3
Alice <m.01233210outlook.com>
2023-10-19 12:36:10 +0900

    Uploaded picture

--- src/app/members/members-data.ts
+++ src/app/members/members-data.ts
@@ -301,4 +301,23 @@ export const members: TMember[] = [
       { name: "Luck", value: 99, max: 100 },
     ],
   },
+  {
+    name: “Michael Peyron”,
+    login: “michael”,
+    role: “M1”,
+    imagePath: “/images/members/michael.jpg”,
+    description: “Its October and there is still a taste of summer in the air”,
+    links: [
+      {
+        name: “Github”,
+        url: “https://github.com/AliceMichael”,
+      },
+    ],
+    stat: [
+      { name: “BTS Jimin is cool”, value: 100, max: 100 },
+      { name: “Capybara”, value: 77, max: 100 },
+      { name: “Stupidity”, value: 100, max: 100 },
+      { name: “Luck, I dont believe in luck”, value: 100, max: 100 },
+    ],
+  },
 ]
