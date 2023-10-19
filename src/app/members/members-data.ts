9af7280e2b47f9a2d869b2d33190eda785c71afc
TsukueRyu <tkalxmk@gmail.com>
2023-10-19 12:43:33 +0900

    Hello nice to meet you.

--- src/app/members/members-data.ts
+++ src/app/members/members-data.ts
@@ -359,4 +359,23 @@ export const members: TMember[] = [
       { name: "Luck", value: 50, max: 100 },
     ],
   },
+  {
+    name: "Mikawa-Mallery Takumi",
+    role: "B1",
+    login: "tsukue",
+    imagePath: "/images/members/tsukue.png",
+    description: "Clueless",
+    links: [
+      {
+        name: "Github",
+        url: "https://github.com/TsukueRyu",
+      },
+    ],
+    stat: [
+      { name: "Strength", value: 0, max: 100 },
+      { name: "Agility", value: 0, max: 100 },
+      { name: "Intelligence", value: 0, max: 100 },
+      { name: "Luck", value: 0, max: 100 },
+    ],
+  },
 ]
