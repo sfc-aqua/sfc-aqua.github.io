b7524dd8439e526dde8fb8d88e6b90d006e8b267
Joe-Xuu <haoranxuu@hotmail.com>
2023-10-19 12:52:16 +0900

    Typo

--- src/app/members/members-data.ts
+++ src/app/members/members-data.ts
@@ -307,14 +307,12 @@ export const members: TMember[] = [
     role: "B1",
     imagePath: "/images/memvers/Joe.jpg",
     description: "May the force be with you",
-    links: [
-        {name: "Github", url:"https://github.com/Joe-Xuu"},
-        ],
+    links: [{ name: "Github", url: "https://github.com/Joe-Xuu" }],
     stat: [
       { name: "Strength", value: 50, max: 100 },
       { name: "Agility", value: 50, max: 100 },
       { name: "Intelligence", value: 50, max: 100 },
       { name: "Luck", value: 50, max: 100 },
     ],
-  }
+  },
 ]
