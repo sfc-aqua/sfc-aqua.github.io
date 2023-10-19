584067a54b6d04441eb689331739c75cc81ef422
Junseo Kwak <jukwak20@student.kis.or.kr>
2023-10-19 15:11:38 +0900

    conflict solved?

--- src/app/members/members-data.ts
+++ src/app/members/members-data.ts
@@ -283,6 +283,64 @@ export const members: TMember[] = [
     ],
   },
   {
+    name: "Austin Huang",
+    login: "austinh",
+    role: "M2",
+    imagePath: "/images/members/austin.png",
+    description: "I do quantum chemistry with quantum computers",
+    links: [
+      {
+        name: "Github",
+        url: "https://github.com/austingmhuang",
+      },
+    ],
+    stat: [
+      { name: "Strength", value: 15, max: 100 },
+      { name: "Agility", value: 25, max: 100 },
+      { name: "Intelligence", value: 45, max: 100 },
+      { name: "Luck", value: 88, max: 100 },
+    ],
+  },
+  {
+    name: "Hikaru Yokomori",
+    login: "voy",
+    role: "B4",
+    imagePath: "/images/members/voy.jpg",
+    description: "Adventuring Science",
+    links: [
+      {
+        name: "Github",
+        url: "https://github.com/Hikaru7-7",
+      },
+    ],
+    stat: [
+      { name: "Strength", value: 30, max: 100 },
+      { name: "Agility", value: 30, max: 100 },
+      { name: "Intelligence", value: 30, max: 100 },
+      { name: "Luck", value: 99, max: 100 },
+    ],
+  },
+  {
+    name: "Shreya Kafle",
+    login: "riya",
+    role: "B2",
+    imagePath: "/images/members/riya.jpg",
+    description: "I'm enjoying my life;)",
+    links: [
+      {
+        name: "Github",
+        url: "https://github.com/s08chan",
+      },
+      { name: "Instagram", url: "https://www.instagram.com/s08j127/" },
+    ],
+    stat: [
+      { name: "Strength", value: 40, max: 100 },
+      { name: "Agility", value: 50, max: 100 },
+      { name: "Intelligence", value: 10, max: 100 },
+      { name: "Luck", value: 80, max: 100 },
+    ],
+  },
+  {
     name: "Tetsuma Ariyoshi",
     login: "mono",
     role: "B1",
