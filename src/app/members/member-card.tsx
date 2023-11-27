b3cd13528d432d950be6568eef82b29997e8127c
PorametPat <43396870+PorametPat@users.noreply.github.com>
2023-11-27 14:19:13 +0900

    Upgrade member page.

--- src/app/members/member-card.tsx
+++ src/app/members/member-card.tsx
@@ -2,10 +2,17 @@ import Link from "next/link"
 import { cn } from "@/components/utils"
 import { TMember } from "./members-data"
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
 const MemberCard = ({ member }: { member: TMember }) => (
   <div
     className={cn(
-      "gap-2 flex flex-col items-center justify-center p-10 bg-base-200 rounded-xl max-w-[400px]"
+      "member-card gap-2 flex flex-col items-center justify-center p-10 bg-base-200 rounded-xl w-[275px] mb-4 break-inside-avoid"
     )}
   >
     <img
@@ -15,39 +22,50 @@ const MemberCard = ({ member }: { member: TMember }) => (
     />
     <div className="text-xl font-bold">{member.name}</div>
     <div className="text-xl font-bold">{member.login}</div>
-    <div className="badge badge-primary rounded-full font-bold">
-      {member.role}
+    <div className="flex flex-wrap gap-1 justify-center">
+      {
+        member.role.map((role, index) => (
+          <span className="badge badge-primary rounded-full font-bold h-min text-center" key={index}>{role}</span>
+        ))
+      }
     </div>
-    <div className="">{member.description}</div>
-    <div className="flex flex-wrap items-center justify-center mt-2">
-      {member.links.map((link, index) => (
-        <Link
-          key={index}
-          className="btn btn-primary btn-sm m-1 rounded-full"
-          href={link.url}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {link.name}
-        </Link>
-      ))}
-    </div>
-    <div className="flex flex-wrap items-center justify-center mt-2">
-      {member.stat?.map((stat, index) => (
-        <div
-          key={index}
-          className="flex flex-col items-center justify-center m-1"
-        >
-          <div className="text-sm">{stat.name}</div>
-          <div className="w-32 h-2 bg-base-300 rounded-full">
-            <div
-              className="h-full bg-primary rounded-full"
-              style={{ width: `${(stat.value / stat.max) * 100}%` }}
-            ></div>
+    <q className="prose">{member.description}</q>
+    <Accordion type="single" collapsible>
+      <AccordionItem value={member.name}>
+        <AccordionTrigger>More details</AccordionTrigger>
+        <AccordionContent className="pl-8">
+          <div className="flex flex-wrap items-center justify-center mt-2">
+            {member.links.map((link, index) => (
+              <Link
+                key={index}
+                className="btn btn-primary btn-sm m-1 rounded-full"
+                href={link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {link.name}
+              </Link>
+            ))}
           </div>
-        </div>
-      ))}
-    </div>
+          <div className="flex flex-wrap items-center justify-center mt-2">
+            {member.stat?.map((stat, index) => (
+              <div
+                key={index}
+                className="flex flex-col items-center justify-center m-1"
+              >
+                <div className="text-sm">{stat.name}</div>
+                <div className="w-32 h-2 bg-base-300 rounded-full">
+                  <div
+                    className="h-full bg-primary rounded-full"
+                    style={{ width: `${(stat.value / stat.max) * 100}%` }}
+                  ></div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   </div>
 )
 
--- src/app/members/members-data.ts
+++ src/app/members/members-data.ts
@@ -1,7 +1,7 @@
 export type TMember = {
   name: string
   login: string
-  role: string
+  role: string[]
   imagePath: string
   description: string
   links: {
@@ -18,7 +18,7 @@ export type TMember = {
 const template: TMember = {
   name: "",
   login: "",
-  role: "",
+  role: [""],
   imagePath: "",
   description: "",
   links: [
@@ -40,7 +40,7 @@ export const members: TMember[] = [
   {
     name: "Rod Van Meter",
     login: "rdv",
-    role: "Chief AQUAnaut",
+    role: ["Chief AQUAnaut", "Current"],
     imagePath:
       "/images/members/rdv-kawaguchiko-2204-recropped-small-avatar.jpg",
     description: "Doing quantum since 2003",
@@ -69,7 +69,7 @@ export const members: TMember[] = [
   },
   {
     name: "Michal Hajdušek",
-    role: "Project Associate Professor",
+    role: ["Project Associate Professor", "Current"],
     login: "michal",
     imagePath: "/images/members/michal.png",
     description: "Engineering is hard!",
@@ -89,7 +89,7 @@ export const members: TMember[] = [
   {
     name: "Shota Nagayama",
     login: "shota",
-    role: "QITF Director",
+    role: ["QITF Director", "Current"],
     description:
       "Love & Peace & Comprehensive effort to realize Quantum Computer Networks.",
     imagePath: "/images/members/shota-nagayama1.jpg",
@@ -121,7 +121,7 @@ export const members: TMember[] = [
   },
   {
     name: "Bernard Ousmane Sane",
-    role: "Project Assistant Professor",
+    role:[ "Project Assistant Professor", "Current" ],
     login: "bernard",
     imagePath: "/images/members/bernard.jpg",
     description: "Distributed Quantum Error Correction ",
@@ -141,7 +141,7 @@ export const members: TMember[] = [
   {
     name: "Amin Taherkhani",
     login: "amin",
-    role: "Project Assistant Professor",
+    role: ["Project Assistant Professor", "Current"],
     imagePath: "/images/members/amin.jpg",
     description: "(|Classical\u27E9+|Quantum\u27E9)/\u221A2 Networking",
     links: [
@@ -163,7 +163,7 @@ export const members: TMember[] = [
   },
   {
     name: "Sara A. Metwalli",
-    role: "D4",
+    role: ["D4", "Current"],
     login: "sara",
     imagePath: "/images/members/sara.JPG",
     description: "I'm a student at Keio University",
@@ -180,7 +180,7 @@ export const members: TMember[] = [
   },
   {
     name: "Naphan Benchasattabuse",
-    role: "D3+",
+    role: ["D3+", "Current"],
     login: "whit3z",
     imagePath: "/images/members/whit3z.png",
     description: "I don't know what I'm doing.",
@@ -199,11 +199,11 @@ export const members: TMember[] = [
     ],
   },
   {
-    name: "Poramet",
-    role: "D1",
+    name: "Poramet Pathumsoot",
+    role: ["D2", "Current"],
     login: "poramet",
     imagePath: "/images/members/poramet.jpg",
-    description: "I'm a student at Keio University",
+    description: "To boldly go where no one has gone before",
     links: [
       { name: "Facebook", url: "" },
       { name: "Github", url: "" },
@@ -218,7 +218,7 @@ export const members: TMember[] = [
   {
     name: "Samanvay Sharma",
     login: "sam",
-    role: "M2",
+    role: ["M2", "Current"],
     imagePath: "/images/members/sam.png",
     description: "Power and Responsibility",
     links: [
@@ -236,7 +236,7 @@ export const members: TMember[] = [
   },
   {
     name: "Makoto Nakai",
-    role: "M2",
+    role: ["M2", "Current"],
     login: "dave",
     imagePath: "/images/members/dave.png",
     description: "I'm a student at Keio University",
@@ -250,7 +250,7 @@ export const members: TMember[] = [
   },
   {
     name: "Yinjie Zhou",
-    role: "M2",
+    role: ["M2", "Current"],
     login: "syu",
     imagePath: "/images/members/syu.png",
     description: "I'm a student at Keio University",
@@ -265,7 +265,7 @@ export const members: TMember[] = [
   {
     name: "Austin Huang",
     login: "austinh",
-    role: "M2",
+    role: ["M2", "Current"],
     imagePath: "/images/members/austin.png",
     description: "I do quantum chemistry with quantum computers",
     links: [
@@ -284,7 +284,7 @@ export const members: TMember[] = [
   {
     name: "Michael Peyron",
     login: "michael",
-    role: "M1",
+    role: ["M1", "Current"],
     imagePath: "/images/members/michael.jpg",
     description: "Its October and there is still a taste of summer in the air",
     links: [
@@ -303,7 +303,7 @@ export const members: TMember[] = [
   {
     name: "Hikaru Yokomori",
     login: "voy",
-    role: "B4",
+    role: ["B4", "Current"],
     imagePath: "/images/members/voy.jpg",
     description: "Adventuring Science",
     links: [
@@ -322,7 +322,7 @@ export const members: TMember[] = [
   {
     name: "YUMOTO Kento",
     login: "ken-tea",
-    role: "B4",
+    role: ["B4", "Current"],
     imagePath: "/images/members/kent_tea.png",
     description: "I wanna go to Peking and Shenzhen.",
     links: [
@@ -340,7 +340,7 @@ export const members: TMember[] = [
   },
   {
     name: "Hyensoo Choi",
-    role: "B3",
+    role: ["B3", "Current"],
     login: "collodi",
     imagePath: "/images/members/collodi.jpg",
     description:
@@ -362,7 +362,7 @@ export const members: TMember[] = [
   },
   {
     name: "Wataru Morishita",
-    role: "B3",
+    role: ["B3", "Current"],
     login: "morishita",
     imagePath: "/images/members/morishita.png",
     description: "The mastermind behind this kenkyukai",
@@ -384,7 +384,7 @@ export const members: TMember[] = [
   },
   {
     name: "Monet Tokuyama",
-    role: "B3",
+    role: ["B3", "Current"],
     login: "bob",
     imagePath: "/images/members/bob.jpg",
     description: "I like sushi",
@@ -401,7 +401,7 @@ export const members: TMember[] = [
   },
   {
     name: "Kento Samuel Soon",
-    role: "B2",
+    role: ["B2", "Current"],
     login: "soon",
     imagePath: "/images/members/soon.png",
     description: "Quantum information seems interesting",
@@ -416,7 +416,7 @@ export const members: TMember[] = [
   {
     name: "Marii Koyama",
     login: "mia",
-    role: "B2",
+    role: ["B2", "Current"],
     imagePath: "/images/members/mia.jpg",
     description: "I'm a student at Keio University",
     links: [
@@ -435,7 +435,7 @@ export const members: TMember[] = [
   {
     name: "Aman Y. Tekleab",
     login: "aman",
-    role: "B2",
+    role: ["B2", "Current"],
     imagePath: "/images/members/darkifywalltemp.jpg",
     description: "I'm me",
     links: [
@@ -455,7 +455,7 @@ export const members: TMember[] = [
   {
     name: "Shreya Kafle",
     login: "riya",
-    role: "B2",
+    role: ["B2", "Current"],
     imagePath: "/images/members/riya.jpg",
     description: "I'm enjoying my life;)",
     links: [
@@ -474,7 +474,7 @@ export const members: TMember[] = [
   },
   {
     name: "Junseo Kwak",
-    role: "B2",
+    role: ["B2", "Current"],
     login: "junseo",
     imagePath: "/images/members/junseo.jpg",
     description: "Student at Keio University",
@@ -489,7 +489,7 @@ export const members: TMember[] = [
   {
     name: "Tetsuma Ariyoshi",
     login: "mono",
-    role: "B1",
+    role: ["B1", "Current"],
     imagePath: "/images/members/mono.png",
     description: "I'm a student at Keio University",
     links: [
@@ -507,7 +507,7 @@ export const members: TMember[] = [
   },
   {
     name: "Mikawa-Mallery Takumi",
-    role: "B1",
+    role: ["B1", "Current"],
     login: "tsukue",
     imagePath: "/images/members/tsukue.png",
     description: "Clueless",
@@ -527,7 +527,7 @@ export const members: TMember[] = [
   {
     name: "Haruka Sakate",
     login: "l0gic",
-    role: "B1",
+    role: ["B1", "Current"],
     imagePath: "/images/members/logic.jpg",
     description: "I'm a student at Keio University",
     links: [
@@ -546,7 +546,7 @@ export const members: TMember[] = [
   {
     name: "Akash prasad",
     login: "pura",
-    role: "B1",
+    role: ["B1", "Current"],
     imagePath: "/images/members/pura.jpg",
     description: "I am a first semester student here",
     links: [
@@ -565,7 +565,7 @@ export const members: TMember[] = [
   {
     name: "Xu Haoran",
     login: "joo",
-    role: "B1",
+    role: ["B1", "Current"],
     imagePath: "/images/members/Joe.jpg",
     description: "May the force be with you",
     links: [{ name: "Github", url: "https://github.com/Joe-Xuu" }],
