acd6c3dc7f2aad1cf5e6539d0e8658de9033cdd4
parton-quark <parton@nii.ac.jp>
2023-11-28 17:09:07 +0000

    add parton to members

--- src/app/members/members-data.ts
+++ src/app/members/members-data.ts
@@ -599,4 +599,23 @@ export const members: TMember[] = [
       { name: "Luck", value: 50, max: 100 },
     ],
   },
+  {
+    name: "Shin Nishio",
+    login: "parton",
+    role: ["Alumni"],
+    imagePath: "/images/members/parton.jpg",
+    description: "I was a member of AQUA from 2017 to 2020, where I worked on system software for quantum computers (compilers and quantum circuit optimization). I received my bachelor's degree from Keio University and am currently a PhD student at Sokendai/NII/OIST.",
+    links: [
+      { name: "Github", url: "https://github.com/parton-quark" },
+      { name: "Google Scholar", url: "https://scholar.google.com/citations?user=gZNt8twAAAAJ&hl=ja"},
+      { name: "Personal web page", url: "https://parton.info/" },
+    ],
+    stat: [
+      {
+        name: "Nonlocality",
+        value: 2,
+        max: 4,
+      },
+    ],
+  }
 ]
