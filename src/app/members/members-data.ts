6679ebbbab7602a8f844269593ccbbb93be4d020
ymbr <37125179+ymbr@users.noreply.github.com>
2023-11-29 02:40:21 +0000

    satoh added photo and updated members description.

--- src/app/members/members-data.ts
+++ src/app/members/members-data.ts
@@ -577,3 +577,25 @@ export const members: TMember[] = [
     ],
   },
 ]
+  {
+    name: "Takahiko Satoh",
+    login: "satoh",
+    role: ["B.A. 2010", "Alumni"],
+    imagePath: "/images/members/satoh.jpg",
+    description: "Let's fly together in a glider.",
+    links: [
+      { name: "Facebook", url: "https://www.facebook.com/takahiko.satoh.75" },
+      { name: "Github", url: "https://github.com/ymbr" },
+      {
+        name: "Google Scholar",
+        url: "https://scholar.google.com/citations?user=7sU5XMMAAAAJ&hl=ja",
+      },
+    ],  
+    stat: [
+      { name: "Strength", value: 30, max: 100 },
+      { name: "Agility", value: 25, max: 100 },
+      { name: "Intelligence", value: 75, max: 100 },
+      { name: "Luck", value: 90, max: 100 },
+    ],
+  },
+]
