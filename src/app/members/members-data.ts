ab9232ab967b4a03d28b0ff588924855cf89e0e0
Bank Natchapol <natchapol.pat@gmail.com>
2025-05-04 00:19:36 +0900

    Update members-data.ts

--- src/app/members/members-data.ts
+++ src/app/members/members-data.ts
@@ -239,6 +239,33 @@ export const members: TMember[] = [
       { name: "Luck", value: 10, max: 100 },
     ],
   },
+   {
+    name: "Natchapol (Bank) Patamawisut",
+    role: ["M2", "Current"],
+    login: "banknatchapol",
+    imagePath: "/images/members/banknatchapol.jpg",
+    description: "Tomorrow has been exhausting.",
+    links: [
+      {
+        name: "Google Scholar",
+        url: "https://scholar.google.com/citations?user=2oKwxZ0AAAAJ",
+      },
+      {
+        name: "Guthib",
+        url: "https://github.com/BankNatchapol"
+      },
+      {
+        name: "Linkedin",
+        url: "https://www.linkedin.com/in/natchapol-patamawisut/"
+      }
+    ],
+    stat: [
+      { name: "Strength", value: 200, max: 100 },
+      { name: "Agility", value: 200, max: 100 },
+      { name: "Intelligence", value: 200, max: 100 },
+      { name: "Sleep", value: 10, max: 100 },
+    ],
+  },
   {
     name: "Samanvay Sharma",
     login: "sam",
