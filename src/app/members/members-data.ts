344c4373443200d3c780ea6583cf98f10b483dd1
larz <lm@keio.jp>
2025-05-08 04:43:53 +0000

    Revert "add larz ٩( 'ω' )و"
    This reverts commit 5d2220ce02c865e9b9f22d816185f4d71041dfc6.

--- src/app/members/members-data.ts
+++ src/app/members/members-data.ts
@@ -798,25 +798,4 @@ export const members: TMember[] = [
       { name: "Weakness", value: 100, max: 100 },
     ],
   },
-  {
-    name: "Lars Matsumiya",
-    login: "larz",
-    role: ["B2", "Current"],
-    imagePath: "images/members/larz.jpg",
-    description: "Bipedal humanoid with a dark purple body, armed with sharp teeth and claws, which it uses to dig through soil.",
-    links: [
-      {
-        name: "Github",
-        url: "https://github.com/master-of-ppap",
-      },
-    ],
-    stat: [
-      { name: "HP", value: 50, max: 255 },
-      { name: "Attack", value: 75, max: 255 },
-      { name: "Defense", value: 75, max: 255 },
-      { name: "Special Attack", value: 65, max: 255 },
-      { name: "Special Defense", value: 65, max: 255 },
-      { name: "Speed", value: 50, max: 255 },
-    ],
-  },
 ]
