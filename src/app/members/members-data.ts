57382046f646f0856073239e6daf2040a0289ad5
yuki kurosawa <kyuryu@yukinoMacBook-Air.local>
2025-05-09 17:43:26 +0900

    add my description

--- src/app/members/members-data.ts
+++ src/app/members/members-data.ts
@@ -836,4 +836,23 @@ export const members: TMember[] = [
       { name: "Luck", value: 80, max: 100 },
     ],
   },
+  {
+    name: "Yuki Kurosawa",
+    login: "kyuryu",
+    role: ["B3", "Current"],
+    imagePath: "images/members/kyuryu.jpg",
+    description: "I'm kyuryu",
+    links: [
+      {
+        name: "GitHub",
+        url: "https://github.com/yuki9638",
+      },
+    ],
+    stat: [
+      { name: "Happiness", value: 100, max: 100 },
+      { name: "Agility", value: 80, max: 100 },
+      { name: "Intelligence", value: 60, max: 100 },
+      { name: "Luck", value: 80, max: 100 },
+    ],
+  },
 ]
