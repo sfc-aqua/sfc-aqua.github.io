0362feefcff26bada0988f7becc8f5147b396d07
kenyas <kenyas@keio.jp>
2025-05-09 18:18:12 +0900

    inserted profile stuff

--- src/app/members/members-data.ts
+++ src/app/members/members-data.ts
@@ -836,4 +836,23 @@ export const members: TMember[] = [
       { name: "Luck", value: 80, max: 100 },
     ],
   },
+  {
+    name: "Kenya Suzuki",
+    login: "kenya",
+    role: ["B3", "Current"],
+    imagePath: "images/members/kenya.jpeg",
+    description: "im from Alaska",
+    links: [
+      {
+        name: "GitHub",
+        url: "https://github.com/kenyas",
+      },
+    ],
+    stat: [
+      { name: "Happiness", value: 96, max: 100 },
+      { name: "Agility", value: 100, max: 100 },
+      { name: "Intelligence", value: 40, max: 100 },
+      { name: "Luck", value: 80, max: 100 },
+    ],
+  },
 ]
