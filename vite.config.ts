a0b4705fea696e1802586ac7e270d392ea651301
PorametPat <43396870+PorametPat@users.noreply.github.com>
2025-05-18 00:35:22 +0900

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { paraglide } from '@inlang/paraglide-sveltekit/vite';
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
+import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
+		tailwindcss(),
 		sveltekit(),
-		paraglide({
+		paraglideVitePlugin({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide'
 		})
