07448e00c4d04c3f6f2bda22278a2af8212fd2a6
Andrew Todd <at@auspicacious.org>
2025-08-26 14:10:34 +0900

    Revert "Lint fixes."
    This reverts commit c79ce12c03034bd7cc2303a04f997a1a0a0e4679.

--- src/app/redirect/qce-2025-keynote/page.tsx
+++ src/app/redirect/qce-2025-keynote/page.tsx
@@ -1,7 +1,5 @@
-import { permanentRedirect } from "next/navigation"
+import { permanentRedirect } from 'next/navigation'
 
 export default function Page() {
-  permanentRedirect(
-    "https://docs.google.com/presentation/d/16pDsMia2eJae-dG3Q93Zpkuf_ONhpcEox1KCjbNzpzA/edit?usp=sharing"
-  )
+  permanentRedirect('https://docs.google.com/presentation/d/16pDsMia2eJae-dG3Q93Zpkuf_ONhpcEox1KCjbNzpzA/edit?usp=sharing')
 }
